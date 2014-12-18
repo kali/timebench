a56976274c14cc705aefbd8db331eea461c9a204
Mathieu Poumeyrol <kali@zoy.org>
2014-12-18 09:59:48 +0100

    graphit pull

--- server/graphite.scala
+++ server/graphite.scala
@@ -11,8 +11,16 @@ import Types._
 
 import com.github.dockerjava.api.DockerClient
 
+import scalaj.http._
+import org.json4s._
+import org.json4s.JsonDSL._
+import org.json4s.jackson.JsonMethods._
+
 object GraphiteStore extends StoreInterface {
   val logger = org.slf4j.LoggerFactory.getLogger(this.getClass)
+  val url = "http://" + Environment.dockerHost
+  val dateFormat = new java.text.SimpleDateFormat("HH:mm'_'yyyyMMdd")
+  dateFormat.setTimeZone(java.util.TimeZone.getTimeZone("UTC"))
 
   def storeValues(timestamp:Date, values:Seq[(Server,Probe,Key,Value)]) {
     val s = new Socket(InetAddress.getByName(Environment.dockerHost), 2003)
@@ -24,7 +32,19 @@ object GraphiteStore extends StoreInterface {
     s.close
   }
 
-  def pullProbe(start:Date, stop:Date, metric:String):List[(Date,Server,Key,Value)] = List()
+  def pullProbe(start:Date, stop:Date, metric:String):List[(Date,Server,Key,Value)] = {
+    val req = Http(url + "/render").params("from" -> dateFormat.format(start), "until" -> dateFormat.format(stop),
+      "target" -> "10sec.*.*.*", "format" -> "json")
+    val resp = parse(StringInput(req.asString.body))
+    resp.asInstanceOf[JArray].values.flatMap {
+      case obj:Map[String,_] =>
+        val Array(_,server,_,key) = obj("target").toString.split('.')
+        obj("datapoints").asInstanceOf[List[List[_]]].flatMap {
+          case v :: ts :: Nil if(v != null) => Some((new Date(1000L*ts.asInstanceOf[BigInt].toLong), server, key, v.asInstanceOf[Double]))
+          case _ => None
+        }
+    }
+  }
 
   import Environment.docker
   import com.github.dockerjava.api.model._
@@ -33,10 +53,10 @@ object GraphiteStore extends StoreInterface {
     val is = docker.buildImageCmd(new java.io.File("docker/graphite")).withTag(image).exec()
     Iterator.continually (is.read).takeWhile(-1 !=).foreach ( GraphiteStore.logger.debug("%s", _) )
     docker.createContainerCmd(image).withName(containerName)
-      .withExposedPorts(new ExposedPort(2003))
+      .withExposedPorts(new ExposedPort(2003), new ExposedPort(80))
       .exec()
     docker.startContainerCmd(containerName).withNetworkMode("bridge")
-      .withPortBindings(PortBinding.parse("2003:2003")).exec()
+      .withPortBindings(PortBinding.parse("2003:2003"), PortBinding.parse("80:80")).exec()
     Retry(103, 10 seconds) { () =>
       new Socket(InetAddress.getByName(Environment.dockerHost), 2003).close()
     }
